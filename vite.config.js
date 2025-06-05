4dcab29ba97a428b1ba21c94606d0231f4e13870
Onyenso Precious <chinoprecious29@gmail.com>
2025-06-05 11:52:28 -0700

    fixed my vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   base: '/bank_mordern_app/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 });
